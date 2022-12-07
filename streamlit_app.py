ad02958ccd457e7a740547d2f4b4118c0d81f804
Weshebi Desmond <dtakwi@gmail.com>
2022-12-07 04:10:18 -0600

    Update streamlit_app.py
    we normalize the response so it can be displayed in table format using panda

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,9 @@ streamlit.header('Fruityvice Fruit Advice!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+#take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruitvice_response.json())
+
+#output the response as a table
+streamlit.dataframe(fruityvice_normalized)
