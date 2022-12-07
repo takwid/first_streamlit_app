01b20eb7219b236bce34a11c5c70d128c4e32a5c
Weshebi Desmond <dtakwi@gmail.com>
2022-12-07 06:23:33 -0600

    Update streamlit_app.py
    inserting data to snowflake table from streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,5 @@ streamlit.dataframe(my_data_rows)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'jackfruit')
 streamlit.write('The user entered', fruit_choice)
+
+mycur.execute("insert into fruit_load_list values ('from streamlit')")
